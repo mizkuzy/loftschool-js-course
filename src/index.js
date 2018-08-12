f0eeb15e1dc4040dd0a00515921ae64ad9e068b9
mizkuzy <MisteR2901>
2018-08-12 14:04:33 +0300

    ex.4

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+    target.click();
 }
 
 /*
