8958158ad68b99124115f53c0f1d0748b5823281
mizkuzy <MisteR2901>
2018-08-18 11:34:41 +0300

    session and local storages

---
+++ src/session-storage/index.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    const myName = document.querySelector('#myName');
+    const bday = document.querySelector('#bday');
+    const about = document.querySelector('#about');
+
+    const save = document.querySelector('#save');
+    const load = document.querySelector('#load');
+    const isSessionStorage = document.querySelector('#isSessionStorage');
+
+    let storage = localStorage;
+
+    save.addEventListener('click', () => {
+        try {
+            storage.data = JSON.stringify({
+                myName: myName.value,
+                bday: bday.value,
+                about: about.value
+            });
+        } catch (e) {
+            alert(`ой вей! Что-то не так! ${e}`);
+        }
+    });
+    load.addEventListener('click', () => {
+        const data = JSON.parse(storage.data || '{}');
+
+        myName.value = data.myName || '';
+        bday.value = data.bday || '';
+        about.value = data.about || '';
+    });
+
+    isSessionStorage.addEventListener('change', () => {
+        storage = isSessionStorage.checked ? sessionStorage : localStorage;
+    });
+
+    // Когда что-то изменяет localStorage происходит событие storage для других открытых на том же домене вкладок
+    window.addEventListener('storage', (event) => {
+        load.click();
+    });
+
+};
