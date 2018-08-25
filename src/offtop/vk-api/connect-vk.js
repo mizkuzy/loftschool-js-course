73199bd99794aff847e2f73959f722f2386fdaf4
mizkuzy <MisteR2901>
2018-08-25 13:36:12 +0300

    get vk friends

--- src/offtop/vk-api/connect-vk.js
+++ src/offtop/vk-api/connect-vk.js
@@ -16,7 +16,42 @@ window.onload = () => {
         });
     }
 
-    auth().then(()=>{
-        console.log('goo')
-    })
+    function callApi(method, params) {
+        params.v = '5.80';
+
+        return new Promise((resolve, reject) => {
+            VK.api(method, params, (data) => {
+                if (data.error) {
+                    reject(data.error);
+                } else {
+                    resolve(data.response);
+                }
+            })
+        })
+    }
+
+    auth()
+        .then(() => {
+            console.log('Auth is resolved');
+
+            return callApi('users.get', {name_case: 'gen'});
+        })
+        .then(([user]) => {
+            console.log(user);
+            const userData = document.querySelector('#userData');
+
+            userData.textContent = `${user.first_name} ${user.last_name}`;
+
+            return callApi('friends.get', {fields: 'city, country, photo_100'})
+        })
+        .then((friends) => {
+            console.log(friends);
+            var source = document.getElementById('user-tmplt').textContent;
+            var render = Handlebars.compile(source);
+            const html = render(friends);
+
+            const querySelector = document.querySelector('#friends-list');
+            querySelector.innerHTML = html
+
+        })
 };
